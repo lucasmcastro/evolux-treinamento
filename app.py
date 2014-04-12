688222f898ea03afea73644478a7c4bdedb66247
Lucas Castro <lucasmcastro@gmail.com>
2014-04-12 11:17:29 -0300

    Alterar e remover vaga

--- app.py
+++ app.py
@@ -28,5 +28,18 @@ def edit_job(job_id):
     return render_template("edit_job.html", job=job)
 
 
+@app.route("/jobs/<job_id>/update", methods=['POST'])
+def update_job(job_id):
+    job = mongo.db.jobs.find_one({'_id': ObjectId(job_id)})
+    job.update(request.form.to_dict())
+    mongo.db.jobs.save(job)
+    return redirect('/')
+
+
+@app.route("/jobs/<job_id>/delete")
+def delete_job(job_id):
+    mongo.db.jobs.remove({'_id': ObjectId(job_id)})
+    return redirect('/')
+
 if __name__ == "__main__":
         app.run(debug=True)
