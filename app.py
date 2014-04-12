64a8cfe885ad0ba899a34e3082177887076d9f8c
Lucas Castro <lucasmcastro@gmail.com>
2014-04-12 10:50:53 -0300

    Alteração de vagas

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import Flask, render_template, request, redirect
 from flask.ext.pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask("jobs")
 mongo = PyMongo(app)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    jobs = mongo.db.jobs.find()
+    return render_template("index.html", jobs=jobs)
 
 
 @app.route("/jobs/new")
@@ -17,7 +19,14 @@ def new_job():
 @app.route("/jobs/create", methods=['POST'])
 def create_job():
     mongo.db.jobs.insert(request.form.to_dict())
-    return redirect('/jobs/new')
+    return redirect('/')
+
+
+@app.route("/jobs/<job_id>/edit")
+def edit_job(job_id):
+    job = mongo.db.jobs.find_one({'_id': ObjectId(job_id)})
+    return render_template("edit_job.html", job=job)
+
 
 if __name__ == "__main__":
         app.run(debug=True)
